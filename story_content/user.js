95f347c54ca54ecfe83100f7a60dce7969f65e3b
sgsure <sgsure1@gmail.com>
2024-05-19 23:51:16 +0300

    first

---
+++ story_content/user.js
@@ -0,0 +1,319 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5w1hYAXfo3B":
+        Script1();
+        break;
+      case "61CjiwC8GGm":
+        Script2();
+        break;
+      case "5qbvR9sLs54":
+        Script3();
+        break;
+      case "5zIfkXyp4b9":
+        Script4();
+        break;
+      case "5YeRSHT5PaF":
+        Script5();
+        break;
+      case "5cjXBh9OlCE":
+        Script6();
+        break;
+      case "65hHpmXQxBH":
+        Script7();
+        break;
+      case "6j18LzPGIRv":
+        Script8();
+        break;
+      case "63f5ewCkucW":
+        Script9();
+        break;
+      case "5dIbpOb1bYj":
+        Script10();
+        break;
+      case "66CairIBMuZ":
+        Script11();
+        break;
+      case "5j9elkwqIoK":
+        Script12();
+        break;
+      case "68CwhRxSDdy":
+        Script13();
+        break;
+      case "5WVnyeyFFfs":
+        Script14();
+        break;
+      case "5XDKO5Vpjji":
+        Script15();
+        break;
+      case "6YILuYcmSaU":
+        Script16();
+        break;
+      case "68ZF406iIcl":
+        Script17();
+        break;
+      case "6O2Vz2fNaKW":
+        Script18();
+        break;
+      case "6qd43ThVgRr":
+        Script19();
+        break;
+      case "6kfRT49vWp9":
+        Script20();
+        break;
+      case "5iMzivjW5La":
+        Script21();
+        break;
+      case "5o8BrTuZBYY":
+        Script22();
+        break;
+      case "6Qe2rzzqRzc":
+        Script23();
+        break;
+      case "5YU5b8pPpWZ":
+        Script24();
+        break;
+      case "6Gce5XO1KEI":
+        Script25();
+        break;
+      case "6INyYmEDIDV":
+        Script26();
+        break;
+      case "6K7DoMnF4Jc":
+        Script27();
+        break;
+      case "6iKb7f3I4JU":
+        Script28();
+        break;
+      case "6Ex3mzAonIj":
+        Script29();
+        break;
+      case "6T0LFIzuOM1":
+        Script30();
+        break;
+      case "674mPauXOGz":
+        Script31();
+        break;
+      case "5Zw0zsWKosQ":
+        Script32();
+        break;
+      case "6GQCNh23Ybe":
+        Script33();
+        break;
+      case "69HOY7u0vkS":
+        Script34();
+        break;
+      case "5YD7nivLS0l":
+        Script35();
+        break;
+      case "5o0xinNQfYJ":
+        Script36();
+        break;
+      case "5VU2adNHQoR":
+        Script37();
+        break;
+      case "6CFWDXiceOc":
+        Script38();
+        break;
+      case "5xZCpYgtVBf":
+        Script39();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print()
+}
+
+function Script2()
+{
+  window.print()
+}
+
+function Script3()
+{
+  window.print()
+}
+
+function Script4()
+{
+  window.print()
+}
+
+function Script5()
+{
+  window.print()
+}
+
+function Script6()
+{
+  window.print()
+}
+
+function Script7()
+{
+  window.print()
+}
+
+function Script8()
+{
+  window.print()
+}
+
+function Script9()
+{
+  window.print()
+}
+
+function Script10()
+{
+  window.print()
+}
+
+function Script11()
+{
+  window.print()
+}
+
+function Script12()
+{
+  window.print()
+}
+
+function Script13()
+{
+  window.print()
+}
+
+function Script14()
+{
+  window.print()
+}
+
+function Script15()
+{
+  window.print()
+}
+
+function Script16()
+{
+  window.print()
+}
+
+function Script17()
+{
+  window.print()
+}
+
+function Script18()
+{
+  window.print()
+}
+
+function Script19()
+{
+  window.print()
+}
+
+function Script20()
+{
+  window.print()
+}
+
+function Script21()
+{
+  window.print()
+}
+
+function Script22()
+{
+  window.print()
+}
+
+function Script23()
+{
+  window.print()
+}
+
+function Script24()
+{
+  window.print()
+}
+
+function Script25()
+{
+  window.print()
+}
+
+function Script26()
+{
+  window.print()
+}
+
+function Script27()
+{
+  window.print()
+}
+
+function Script28()
+{
+  window.print()
+}
+
+function Script29()
+{
+  window.print()
+}
+
+function Script30()
+{
+  window.print()
+}
+
+function Script31()
+{
+  window.print()
+}
+
+function Script32()
+{
+  window.print()
+}
+
+function Script33()
+{
+  window.print()
+}
+
+function Script34()
+{
+  window.print()
+}
+
+function Script35()
+{
+  window.print()
+}
+
+function Script36()
+{
+  window.print()
+}
+
+function Script37()
+{
+  _g.trigger("volume:changed", GetPlayer().GetVar("Volume"));
+}
+
+function Script38()
+{
+  window.print()
+}
+
+function Script39()
+{
+  _g.trigger("volume:changed", GetPlayer().GetVar("Volume"));
+}
+
